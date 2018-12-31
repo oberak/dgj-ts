8d8aee23d152f59c68f85f3ab5f8a5c0d378ec5d
Gilho Won <kiaora@Gilhos-MacBook-Pro.local>
2018-12-31 09:24:38 +0630

    Add vuetify

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.css';
 
 import App from './App';
 import router from './router';
 import store from './store';
 
+Vue.use(Vuetify);
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.config.productionTip = false;
 
